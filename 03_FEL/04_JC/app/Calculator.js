ed3b71952e648d7f555645629943b89a766b3c9f
Philip Gutbrodt <p.gutbrodt@gmail.com>
2019-02-11 12:50:07 +0100

    [04_JC] Calculator Layout incl. all Buttons

--- 03_FEL/04_JC/app/Calculator.js
+++ 03_FEL/04_JC/app/Calculator.js
@@ -9,10 +9,13 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var Calculator = function (_React$Component) {
     _inherits(Calculator, _React$Component);
 
-    function Calculator() {
+    function Calculator(props) {
         _classCallCheck(this, Calculator);
 
-        return _possibleConstructorReturn(this, (Calculator.__proto__ || Object.getPrototypeOf(Calculator)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (Calculator.__proto__ || Object.getPrototypeOf(Calculator)).call(this, props));
+
+        _this.state = {};
+        return _this;
     }
 
     _createClass(Calculator, [{
@@ -22,9 +25,25 @@ var Calculator = function (_React$Component) {
                 "div",
                 { id: "calculator" },
                 React.createElement(Display, null),
-                React.createElement(Button, { className: "button number-button", content: "1" }),
                 React.createElement(Button, { className: "button utility-button", content: "AC" }),
-                React.createElement(Button, { className: "button math-button", content: "+" })
+                React.createElement(Button, { className: "button utility-button", content: "+/-" }),
+                React.createElement(Button, { className: "button utility-button", content: "%" }),
+                React.createElement(Button, { className: "button math-button", content: "\xF7" }),
+                React.createElement(Button, { className: "button number-button", content: "7" }),
+                React.createElement(Button, { className: "button number-button", content: "8" }),
+                React.createElement(Button, { className: "button number-button", content: "9" }),
+                React.createElement(Button, { className: "button math-button", content: "x" }),
+                React.createElement(Button, { className: "button number-button", content: "4" }),
+                React.createElement(Button, { className: "button number-button", content: "5" }),
+                React.createElement(Button, { className: "button number-button", content: "6" }),
+                React.createElement(Button, { className: "button math-button", content: "-" }),
+                React.createElement(Button, { className: "button number-button", content: "1" }),
+                React.createElement(Button, { className: "button number-button", content: "2" }),
+                React.createElement(Button, { className: "button number-button", content: "3" }),
+                React.createElement(Button, { className: "button math-button", content: "+" }),
+                React.createElement(Button, { id: "zero", className: "button", content: "0" }),
+                React.createElement(Button, { className: "button number-button", content: "." }),
+                React.createElement(Button, { className: "button math-button", content: "=" })
             );
         }
     }]);
@@ -45,7 +64,7 @@ var Display = function (_React$Component2) {
         key: "render",
         value: function render() {
             return React.createElement(
-                "h1",
+                "div",
                 { id: "display" },
                 "Display"
             );
@@ -69,7 +88,7 @@ var Button = function (_React$Component3) {
         value: function render() {
             return React.createElement(
                 "button",
-                { className: this.props.className },
+                { className: this.props.className, id: this.props.id },
                 this.props.content
             );
         }
--- 03_FEL/04_JC/src/Calculator.js
+++ 03_FEL/04_JC/src/Calculator.js
@@ -2,13 +2,36 @@
 
 class Calculator extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state ={
+
+        };
+    }
+
     render() {
         return (
             <div id="calculator">
                 <Display/>
-                <Button className="button number-button" content="1"/>
                 <Button className="button utility-button" content="AC"/>
+                <Button className="button utility-button" content="+/-"/>
+                <Button className="button utility-button" content="%"/>
+                <Button className="button math-button" content="÷"/>
+                <Button className="button number-button" content="7"/>
+                <Button className="button number-button" content="8"/>
+                <Button className="button number-button" content="9"/>
+                <Button className="button math-button" content="x"/>
+                <Button className="button number-button" content="4"/>
+                <Button className="button number-button" content="5"/>
+                <Button className="button number-button" content="6"/>
+                <Button className="button math-button" content="-"/>
+                <Button className="button number-button" content="1"/>
+                <Button className="button number-button" content="2"/>
+                <Button className="button number-button" content="3"/>
                 <Button className="button math-button" content="+"/>
+                <Button id="zero" className="button" content="0"/>
+                <Button className="button number-button" content="."/>
+                <Button className="button math-button" content="="/>
             </div>
         )
     }
@@ -19,7 +42,7 @@ class Calculator extends React.Component {
 class Display extends React.Component{
     render() {
         return(
-            <h1 id="display">Display</h1>
+            <div id="display">Display</div>
         )
     }
 }
@@ -29,7 +52,7 @@ class Button extends React.Component{
 
     render(){
         return(
-            <button className={this.props.className}>{this.props.content}</button>
+            <button className={this.props.className} id={this.props.id}>{this.props.content}</button>
         );
     }
 
